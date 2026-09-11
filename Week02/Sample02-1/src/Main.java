b69733216ebc9baf972f3d787768d16e1445f242
gmiso0012 <gmiso0012@kbu.ac.kr>
2026-09-11 13:58:40 +0900

    2주차 수업 내용

---
+++ Week02/Sample02-1/src/Main.java
@@ -0,0 +1,15 @@
+// 이 프로그램은 처음 실습 내용입니다.
+
+/*
+    이 프로그램은 처음 실습 내용입니다.
+ */
+
+
+void main() {
+    String name = "이순신";    // 문자열 데이터를 선언하는 선언문
+
+    System.out.printf("이름 : %s\n", name);
+    System.out.print("대한민국\n");
+    System.out.println("경복대학교");
+    System.out.println("홍길동");
+}
---
+++ Week02/Sample02-2/src/Main.java
@@ -0,0 +1,15 @@
+
+
+void main() {
+    Scanner keyboard = new Scanner(System.in);
+    String name;
+    int age;
+
+    System.out.print("당신의 이름은 ? "); // prompt 문장
+    name = keyboard.nextLine();
+    System.out.printf("%s님의 나이는 ? ", name); // prompt 문장
+    age = keyboard.nextInt();
+
+
+    System.out.printf("이름 : %s, 나이 : %d\n", name, age);
+}
