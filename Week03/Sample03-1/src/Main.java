36a9c84730735421fe35697207b981a68da6a088
gmiso0012 <gmiso0012@kbu.ac.kr>
2026-09-18 14:13:53 +0900

    3주차 수업 내용

---
+++ Week03/Sample03-1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+void main() {
+    boolean flag; // false;
+    byte value1 = -128;     // -128 ~ 127
+    short value2 = 32767;       // -32,768 ~ 32,767
+    int value3 = 444444444;     // -2^63 ~ 2^63 - 1
+    long value4 = 44444444L;        // L자를 써주면 더 큰 수도 입력 가능
+    double value5 = 4.555555555555555555;
+    float value6 = 4.55555555555555555f;
+    char value7 = 'A';
+    String value8 = "AB";
+}
